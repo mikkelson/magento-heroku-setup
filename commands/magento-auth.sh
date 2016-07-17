2424e094f25bcdbe65bb491d51941bf8696a9dc9
Yevhen V <wik@bdhr.co>
2016-07-17 02:53:54 +0100

    Fix user input in magento auth script

--- commands/magento-auth.sh
+++ commands/magento-auth.sh
@@ -3,8 +3,8 @@
 # Enable access to magento repository
 cp assets/magento-auth.json $MAGENTO_PATH/auth.json
 cd $MAGENTO_PATH
-read -p read 'Username:' username
-read -p read 'Password:' password
+read -p 'username:' username
+read -p 'password:' password
 sed -i -- "s/replace-username/$username/g" auth.json
 sed -i -- "s/replace-password/$password/g" auth.json
 git add .
