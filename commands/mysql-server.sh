d2a2c52d6a419769f19e3e5c09c78b77ac8f43ea
Yevhen V <wik@bdhr.co>
2016-07-17 02:54:21 +0100

    Fix path to mysql privileges asset

--- commands/mysql-server.sh
+++ commands/mysql-server.sh
@@ -5,5 +5,5 @@ SERVER_IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 
 # Configure server
 sed -i -- "s/127\.0\.0\.1/$MAGENTO_SERVER_IP/g" /etc/mysql/my.cnf
-mysql < magento-privileges.sql
+mysql < assets/magento-privileges.sql
 service mysql restart
